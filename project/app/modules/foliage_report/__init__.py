19edc4cec1e5cd490cf8191519590d7f49c0ac09
Johnny De Castro <jdcastro@unweb.co>
2025-06-11 16:59:09 -0500

    Fix foliage_report blueprint formatting

--- project/app/modules/foliage_report/__init__.py
+++ project/app/modules/foliage_report/__init__.py
@@ -1,10 +1,9 @@
 from flask import Blueprint
 
-foliage_report = Blueprint(
-    "foliage_report", __name__, url_prefix="/dashboard/foliage_report", template_folder="templates"
-)
-foliage_report_api = Blueprint(
-    "foliage_report_api", __name__, url_prefix="/api/foliage/report"
-    )
+foliage_report = Blueprint("foliage_report", __name__,
+                            url_prefix="/dashboard/foliage_report",
+                            template_folder="templates")
+foliage_report_api = Blueprint("foliage_report_api", __name__,
+                               url_prefix="/api/foliage/report")
 
 from . import web_routes, api_routes
